2acdb995cfaa01055524373debdd0b78674060fb
Titobioluwa Dada <Titobioluwad@gmail.com>
2026-07-04 02:10:56 -0230

    added groovy scripts

---
+++ vars/buidimage.groovy
@@ -0,0 +1,9 @@
+#!/user/bin/env groovy
+
+def call() {
+    echo "building docker image.. "
+    withCredentials([usernamePassword(credentialsId: '', passwordVariable: 'PASS', usernameVariable:'USER')])
+    sh "docker build -t titobid/jenkins-app:1.0"
+    sh "echo $PASS | docker login -u $USER --password-stdin"
+    sh "docker push titobid/jenkins-app:1.0"
+}
