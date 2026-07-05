35a2193771af8e6c47cd67d8d14b7f9171e6f649
Titobioluwa Dada <Titobioluwad@gmail.com>
2026-07-05 01:11:22 -0230

    added git-hub credentials

--- vars/buidimage.groovy
+++ vars/buidimage.groovy
@@ -2,7 +2,7 @@
 
 def call() {
     echo "building docker image.. "
-    withCredentials([usernamePassword(credentialsId: '', passwordVariable: 'PASS', usernameVariable:'USER')])
+    withCredentials([usernamePassword(credentialsId: 'github-cred', passwordVariable: 'PASS', usernameVariable:'USER')])
     sh "docker build -t titobid/jenkins-app:1.0"
     sh "echo $PASS | docker login -u $USER --password-stdin"
     sh "docker push titobid/jenkins-app:1.0"
